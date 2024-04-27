7a6044aea427289d1f49049fca016c41ad09800c
SunlightWings <87812066+SunlightWings@users.noreply.github.com>
2024-04-27 10:56:26 +0545

    t = 57%, s =86%

--- Roman_to_Integer/first.py
+++ Roman_to_Integer/first.py
@@ -4,28 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Define a dictionary to map Roman numerals to their values
-        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-        result = 0
-        prev_value = 0  # To keep track of the previous numeral's value
-
-        # Iterate through the Roman numeral string in reverse
-        for char in reversed(s):
-            current_value = roman_dict[char]
-
-            # Check if we need to subtract the current value
-            if current_value < prev_value:
-                result -= current_value
+        answer = 0
+        previous = 0
+        reverse = s[::-1]                         # reverse the string. (to subtract if the small comes first)
+        dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        
+        for char in reverse:
+            if dict[char] < previous:             # dict[char] = value of the key 'char'
+                answer = answer - dict[char]
             else:
-                result += current_value
-
-            prev_value = current_value
-
-        return result
-
-# Example usage
-solution = Solution()
-print(solution.romanToInt("III"))       # Output: 3
-print(solution.romanToInt("LVIII"))     # Output: 58
-print(solution.romanToInt("MCMXCIV"))   # Output: 1994
+                answer = answer + dict[char]
+            previous = dict[char]
+        return answer
+            
